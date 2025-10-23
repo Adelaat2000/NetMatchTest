2976f5d21e8f1ce39622d40bc456a2c42a3995e5
Rik Jansen <84772073+g00gleman@users.noreply.github.com>
2025-10-23 11:21:45 +0200

    added offerte classe met service en mapper

---
+++ NetMatch.Logic/Mappers/OfferteMapper.cs
@@ -0,0 +1,17 @@
+﻿using NetMatch.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetMatch.Logic.Mappers
+{
+    public class OfferteMapper
+    {
+        public OfferteClass toClass(int id, string naam)
+        {
+            return new OfferteClass(id, naam);
+        }
+    }
+}
