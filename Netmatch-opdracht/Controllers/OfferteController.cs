2240593e8bec7477ee98a07121c7ba8c8a03aad8
I569189 <I569189@student.fontys.nl>
2025-10-29 13:50:52 +0100

    Dashboard view en controller gemaakt met fictieve servicenaam.

---
+++ Netmatch-opdracht/Controllers/OfferteController.cs
@@ -0,0 +1,23 @@
+using Netmatch_opdracht.Models;
+
+namespace Netmatch_opdracht.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+public class OfferteController : Controller
+{
+    private readonly OfferteService _offerteService;
+
+    public OfferteController(OfferteService offerteService)
+    {
+        _offerteService = offerteService;
+    }
+    
+    [HttpPost]
+    public IActionResult Create(CreateOfferteViewModel model)
+    {
+        //naam moet gecorigeerd worden.
+        _offerteService.Create(model);
+        
+        return RedirectToAction("Index", "Home");
+    }
+}
