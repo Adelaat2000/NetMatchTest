5b19595976fbc376b48070780df5b971872c70ea
Riceu <rclsung@Gmail.com>
2025-10-29 14:16:40 +0100

    DBConnection + ConString
    Connectionstring staat in appsettings + DBConnection kan worden gemaakt.
    Later verder kijken naar doorgestuurde code van Rik

---
+++ NetMatch.DAL/DAL/DbConnection.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using SqlConnection = Microsoft.Data.SqlClient.SqlConnection;
+
+namespace NetMatch.DAL.DAL;
+
+public class DbConnection
+{
+    private readonly string? _connectionString;
+
+    //fetch connection string from appsettings.json
+    public DbConnection(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("NetmatchDB");
+        if (string.IsNullOrEmpty(_connectionString))
+        {
+            throw new InvalidOperationException("Connection string 'NetmatchDB' not found.");
+        }
+    }
+
+    //open and return a new SqlConnection
+    public SqlConnection GetConnection()
+    {
+        var connection = new SqlConnection(_connectionString);
+        connection.Open();
+        return connection;
+    }
+}
