68418159972a032fe898eb8c2c546da87a3c52bd
I569189 <I569189@student.fontys.nl>
2025-11-06 11:10:26 +0100

    Frontend offertebuilder aangemaakt

---
+++ Netmatch-opdracht/Models/ViewModels/OfferteBuilderViewModel.cs
@@ -0,0 +1,21 @@
+namespace Netmatch_opdracht.Models.ViewModels
+{
+    public class OfferteBuilderViewModel
+    {
+        public string ItineraryName { get; set; } = "Parisian Dreams Itinerary";
+        public string Destination { get; set; } = "Paris, Frankrijk";
+        public string SelectedCategory { get; set; } = "accommodations"; // accommodations, activities, transportation
+        public decimal TotalPrice { get; set; } = 0;
+        public List<SelectedComponent> SelectedComponents { get; set; } = new List<SelectedComponent>();
+    }
+
+    public class SelectedComponent
+    {
+        public string Type { get; set; } = ""; // ACCOMMODATIE, VERVOER, ACTIVITEIT
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public decimal Price { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Details { get; set; }
+    }
+}
